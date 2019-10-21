9fdeac95a5dfa722b8edcf3f9e02c28cf7b54edc
Owen Cruse <ocruse7@gmail.com>
2019-10-21 19:13:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(0)
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop(array[-1])
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
