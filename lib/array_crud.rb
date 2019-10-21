458288d060e63de146beeb9a74147a72a76f34a4
Owen Cruse <ocruse7@gmail.com>
2019-10-21 18:35:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Wow!", "I", "am"]
+  array = ["wow", "I", "am", "really", "learning"]
+  
 end
 
 def add_element_to_start_of_array(array, element)
